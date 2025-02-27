2b44b37571653d3acd2c2431374a6ccee803ade2
fsahnouni <fsahnoun96@gmail.com>
2025-02-26 21:55:02 -0600

    added values for microsoft and nvidia stock tickets

--- src/App.js
+++ src/App.js
@@ -7,13 +7,19 @@ import axios from 'axios';
 import StockInfo from './Components/StockInfo';
 
 function App() {
-  const [stockData, setStockData] = useState(null);
+  const [aaplData, setaaplData] = useState(null);
+  const [msftData, setmsftData] = useState(null);
+  const [nvdaData, setnvdaData] = useState(null);
+
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get('http://api.marketstack.com/v1/eod?access_key=745e981c5b38d30700cfad03eec01af8&symbols=AAPL');
-      console.log(result.data.data);
-      setStockData(result.data.data);
+      const aapl = await axios.get('http://api.marketstack.com/v1/eod?access_key=745e981c5b38d30700cfad03eec01af8&symbols=AAPL');
+      setaaplData(aapl.data.data);
+      const msft = await axios.get('http://api.marketstack.com/v1/eod?access_key=745e981c5b38d30700cfad03eec01af8&symbols=MSFT');
+      setmsftData(msft.data.data); 
+      const nvda = await axios.get('http://api.marketstack.com/v1/eod?access_key=745e981c5b38d30700cfad03eec01af8&symbols=NVDA');
+      setnvdaData(nvda.data.data);
     };
     fetchData();
   }, []);
@@ -21,7 +27,12 @@ function App() {
   return (
     <div className="App">
       <h1>Stock Market Info</h1>
-      {stockData && <StockInfo stockData={stockData} />}
+      {aaplData && <StockInfo stockData={aaplData} />}
+      <br />
+      {msftData && <StockInfo stockData={msftData} />}
+      <br />
+      {nvdaData && <StockInfo stockData={nvdaData} />}
+
     </div>
   );
 }
